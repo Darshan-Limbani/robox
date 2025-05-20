dd1fa025dc3387caf4792d38943575bab51aff0c
Darshan Limbani <darshanlimbani94@gmail.com>
2025-05-20 23:44:04 +0530

    updated script

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import { RouterProvider } from "react-router-dom";
 import routers from "./Router/Routes.jsx";
 import PageWrapper from "./Pages/PageWrapper.jsx";
 import { PointsProvider } from "./PointsProvider.jsx";
+import { useEffect } from "react";
 
 function App() {
-  
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://securepubads.g.doubleclick.net/tag/js/gpt.js';
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <PointsProvider>
       <PageWrapper>
