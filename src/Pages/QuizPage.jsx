12fe4fe12089d2fd4b8eeed7cf7a7d032c71dad2
Darshan Limbani <darshanlimbani94@gmail.com>
2025-06-29 15:34:42 +0530

    updated config

--- src/Pages/QuizPage.jsx
+++ src/Pages/QuizPage.jsx
@@ -2,7 +2,7 @@ import ProfileHeader from "../Components/ProfileHeader.jsx";
 import Quiz from "../Components/Quiz.jsx";
 import back_arr from "../assets/back_arr.svg";
 
-function QuizPage(props) {
+function QuizPage() {
   return (
     <main
       className="flex flex-col h-full text-black bg-white w-full max-w-[360px]">
